f150c34d121dde66159f24a97abfebe5cb2f7780
Aleksey K <aleksey.k.dev@gmail.com>
2020-07-03 04:42:55 +0300

    Fixed build error

--- src/app/animations/slide.ts
+++ src/app/animations/slide.ts
@@ -1,36 +1,38 @@
 import { trigger, transition, animate, style } from '@angular/animations';
 
-export const slide = (duration = '0.3s') => trigger('slide', [
-    transition(':enter', [
-        style({
-            height: 0,
-            paddingTop: 0,
-            paddingBottom: 0,
-            opacity: 0,
-            overflow: 'hidden'
-        }),
-        animate(
-            duration,
-            style({
-                height: '*',
-                opacity: '*',
-                paddingTop: '*',
-                paddingBottom: '*'
-            })
-        )
-    ]),
-    transition(':leave', [
-        style({
-            overflow: 'hidden'
-        }),
-        animate(
-            duration,
+export function slide(duration = '0.3s') {
+    return trigger('slide', [
+        transition(':enter', [
             style({
                 height: 0,
                 paddingTop: 0,
                 paddingBottom: 0,
-                opacity: 0
-            })
-        )
-    ])
-]);
+                opacity: 0,
+                overflow: 'hidden'
+            }),
+            animate(
+                duration,
+                style({
+                    height: '*',
+                    opacity: '*',
+                    paddingTop: '*',
+                    paddingBottom: '*'
+                })
+            )
+        ]),
+        transition(':leave', [
+            style({
+                overflow: 'hidden'
+            }),
+            animate(
+                duration,
+                style({
+                    height: 0,
+                    paddingTop: 0,
+                    paddingBottom: 0,
+                    opacity: 0
+                })
+            )
+        ])
+    ]);
+}
